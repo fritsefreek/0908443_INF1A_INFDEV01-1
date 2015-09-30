398e4867782aa9db7697563e72a70c0582ef5120
Remco Visser <0908443@hr.nl>
2015-09-30 11:01:04 +0200

    Exercise 1

---
+++ INFDEV01-1/assignment5/exercise1/exercise1/exercise1/exercise1.py
@@ -0,0 +1,9 @@
+string = raw_input("Please fill in a string: ")
+characters = len(string)
+reversedString = ""
+
+while characters > 0:
+    reversedString += string[characters -1]
+    characters -= 1
+
+print(reversedString)
