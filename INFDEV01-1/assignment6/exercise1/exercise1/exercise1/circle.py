10fa3b4d2c792e5b06f05d0cdf478712f565e0ed
= <rvremco@hotmail.com>
2015-10-04 14:01:19 +0200

    Circel V1

--- INFDEV01-1/assignment6/exercise1/exercise1/exercise1/circle.py
+++ INFDEV01-1/assignment6/exercise1/exercise1/exercise1/circle.py
@@ -1,3 +1,18 @@
-﻿int = int(raw_input("Please fill in a number: "))
-figure = "test"
+﻿import math
 
+int = int(raw_input("Please fill in a number: "))
+radius = int / 2
+figure = ""
+
+for i in range(1, int):
+    x1y1 = (i - (radius)) * (i - (radius))
+    
+    for x in range(1, int):
+        x2y2 = (x - (radius)) * (x - (radius))
+        if math.sqrt(x1y1 + x2y2) < radius:
+            figure += "*"
+        else:
+            figure += " "
+    figure += "\n"
+
+print(figure)
