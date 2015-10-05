62d7bea1ca2194d29107ab208c9647823177e4e0
= <rvremco@hotmail.com>
2015-10-05 21:20:24 +0200

    Exercise 3

--- INFDEV01-1/assignment5/exercise3/exercise3/exercise3/exercise3.py
+++ INFDEV01-1/assignment5/exercise3/exercise3/exercise3/exercise3.py
@@ -1,10 +1,23 @@
-string = raw_input("Please fill in a string: ")
+﻿string = raw_input("Please fill in a string: ")
 int = int(raw_input("Please fill in an int: "))
 newString = ""
 
 for character in string:
-    if character.isalpha() :
-        character = chr(ord(character) + int)
-    newString += character
+    initalOrdValue = ord(character)
+    
+    if character.isalpha():
+        # Lower
+        if character.islower():
+            if ord(character) + int > 122:
+                character = chr(96 + int) 
+            else:
+                character = chr(123 + int)
+        # Capital
+        else:
+            if ord(character) + int > 90:
+                character = chr(64 + int) 
+            else:
+                character = chr(91 + int) 
 
+    newString += character
 print(newString)
