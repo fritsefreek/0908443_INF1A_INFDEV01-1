17b12f7e5f224ab8052788723c093603f0a90c53
= <rvremco@hotmail.com>
2015-10-05 14:03:24 +0200

    Smily scaling

--- INFDEV01-1/assignment6/exercise1/exercise1/exercise1/smiley.py
+++ INFDEV01-1/assignment6/exercise1/exercise1/exercise1/smiley.py
@@ -1,33 +1,35 @@
-﻿figure = ""
+﻿size = int(raw_input("Size: "))
+scale = size / 10
+figure = ""
 
-for i in range(0, 10):
-    for x in range(0, 10):
+for i in range(0, size):
+    for x in range(0, size):
         # Border
-        if (i == 0 or i == 8) and (x == 3 or x == 4 or x == 5):
+        if (i == (0 * scale) or i == (8 * scale)) and (x == (3 * scale) or x == (4 * scale) or x == (5 * scale)):
             figure += "*"
-        elif (i == 1 or i == 7 ) and (x == 2 or x == 6):
+        elif (i == (1 * scale) or i == (7 * scale)) and (x == (2 * scale) or x == (6 * scale)):
             figure += "*"
-        elif (i == 2 or i == 6) and (x == 1 or x == 7):
+        elif (i == (2 * scale) or i == (6 * scale)) and (x == (1 * scale) or x == (7 * scale)):
             figure += "*"
-        elif (i == 3 or i == 4 or i == 5) and (x == 0 or x == 8):
+        elif (i == (3 * scale) or i == (4 * scale) or i == (5 * scale)) and (x == (0 * scale) or x == (8 * scale)):
             figure += "*"
         # Eyebrows
-        elif i == 2 and (x == 2 or x == 6):
+        elif i == (2 * scale) and (x == (2 * scale) or x == (6 * scale)):
             figure += "-"
         # Eyes
-        elif i == 3 and (x == 2 or x == 6):
+        elif i == (3 * scale) and (x == (2 * scale) or x == (6 * scale)):
             figure += "o"
         # Nose
-        elif i == 5 and x == 4:
+        elif i == (5 * scale) and x == (4 * scale):
             figure += "#"
         # Cheek left
-        elif i == 5 and x == 2:
+        elif i == (5 * scale) and x == (2 * scale):
             figure += "\\"
         # Cheek right
-        elif i == 5 and x == 6:
+        elif i == (5 * scale) and x == (6 * scale):
             figure += "/"
         # Mouth
-        elif i == 6 and (x == 3 or x == 4 or x == 5):
+        elif i == (6 * scale) and (x == (3 * scale) or x == (4 * scale) or x == (5 * scale)):
             figure += "-"
         else:
             figure += " "
