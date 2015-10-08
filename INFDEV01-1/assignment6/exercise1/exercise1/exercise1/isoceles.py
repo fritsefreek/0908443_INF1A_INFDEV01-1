6280a42a95e56c948273e340be746c3271a76e58
Remco Visser <0908443@hr.nl>
2015-10-08 19:08:56 +0200

    idk

--- INFDEV01-1/assignment6/exercise1/exercise1/exercise1/isoceles.py
+++ INFDEV01-1/assignment6/exercise1/exercise1/exercise1/isoceles.py
@@ -1,24 +1,6 @@
 ﻿int = int(raw_input("Please fill in a number: "))
-spaces = int
-stars = 1
 figure = ""
 
-for i in range(0, int):
-
-    spaces -= 1
-
-    for space in range(0, spaces):
-      figure += " "
-    
-    for star in range(0, stars):
-        figure += "*"
-
-    stars += 2
-    figure += "\n"
-print(figure)
-
-
-figure = ""
 for i in range(0, int):
     for x in range(0, int + i):
         if x < int - (i + 1):
