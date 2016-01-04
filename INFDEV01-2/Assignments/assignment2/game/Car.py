7dc30f5f36fdc94147792b2747d115d122f4112f
Remco Visser <rvremco@hotmail.com>
2016-01-04 13:11:22 +0100

    Assignment 2 first commit

---
+++ INFDEV01-2/Assignments/assignment2/game/Car.py
@@ -0,0 +1,25 @@
+﻿import pygame
+import random
+from Node import *
+from Common import *
+
+class Car:
+    def __init__(self, tile):
+        self.Tile = tile
+
+def DrawCars(car, screen, offset, car_texture):
+    _width = int(offset / 3)
+
+    screen.blit(pygame.transform.scale(car_texture, (_width, _width)),
+        (_width + car.Value.Tile.Position.X * offset,
+        _width + car.Value.Tile.Position.Y * offset))
+
+def UpdateCars(cars, updatedCars):
+    print(cars.Value.Tile.Down.Traverseable)
+    newPosition = False
+    if(cars.Value.Tile.Down.Traverseable == True):
+        newPosition = cars.Value.Tile.Down
+
+        if(newPosition != None):
+            updatedCars = Node(Car(newPosition), updatedCars)
+    return updatedCars
---
+++ INFDEV01-2/Assignments/assignment2/game/Game.py
@@ -0,0 +1,62 @@
+﻿import time
+from threading import Thread
+import os, pygame
+import time
+from Tile import *
+from Node import *
+from Boat import *
+pygame.init()
+size = width, height = 600, 600
+white = 255, 255, 255
+green = 50, 255, 100
+screen = pygame.display.set_mode(size)
+offset = 50
+size = 10
+entry_road, entry_rivers, bridges = build_scene(size, offset)
+boat_texture = pygame.image.load("Content/tanker.png").convert_alpha()
+car_texture = pygame.image.load("Content/car.png").convert_alpha()
+cars = Empty
+
+def Update(cars):
+    updatedCars = Empty
+    while cars.IsEmpty is False:
+        updatedCars = UpdateCars(cars, updatedCars)
+        cars = cars.Tail
+    return updatedCars
+
+
+def Draw(cars, screen):
+    while not cars.IsEmpty:
+        DrawCars(cars, screen, offset, car_texture)
+        cars = cars.Tail
+
+
+def Main(cars):
+  counter = 0
+  while True:    
+    counter += 1
+    if counter == 5:
+        cars = Node(Car(entry_road.Value), cars)
+        counter = 0
+    screen.fill(green)
+
+    #here we draw the board, do not move
+    _board = entry_road
+    while not _board.IsEmpty:
+      _board.Value.Draw(screen, False)
+      _board = _board.Tail
+
+    #here we draw the bridges, do not move
+    _board = bridges
+    while not _board.IsEmpty:
+      _board.Value.Draw(screen, True)
+      _board = _board.Tail
+
+
+    cars = Update(cars)
+    Draw(cars, screen)
+
+    pygame.display.flip()
+    time.sleep(0.2)
+    
+Main(cars)
